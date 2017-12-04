437505079609805b046c762c4db5f73e5ef25c18
Leon Talbot <leontalbot@gmail.com>
2017-12-04 10:16:46 -0500

    clojars update

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://github.com/leontalbot/pull"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :deploy-repositories [["releases"  {:sign-releases false :url "https://clojars.org/pull"}]
+                        ["snapshots" {:sign-releases false :url "https://clojars.org/pull"}]])
